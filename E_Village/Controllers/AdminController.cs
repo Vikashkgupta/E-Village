daae93569d21dd8ffd79a1affb367017cdf0d9f3
Vikashkgupta <v43400089@gmail.com>
2024-01-05 23:10:43 +0530

    first commit

---
+++ E_Village/Controllers/AdminController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_Village.Models;
+
+namespace E_Village.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        E_VillageEntities db = new E_VillageEntities();
+
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Log In first. Then go to next zone . '); window.location.href='/home/admin_login' </script>");
+            }
+            return View();
+        }
+        public ActionResult RegistrationMGMT()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Log In first. Then go to next zone . '); window.location.href='/home/admin_login' </script>");
+            }
+
+            List<Table_Registration> rege = null;
+            rege = db.Table_Registration.ToList();
+
+            return View(rege);
+        }
+        public ActionResult ContactMGMT()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Log In first. Then go to next zone . '); window.location.href='/home/admin_login' </script>");
+            }
+
+            List<Table_Contact> lst = null;
+            lst = db.Table_Contact.ToList();
+
+            return View(lst);
+        }
+
+        public ActionResult FeedbackMGMT()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Log In first. Then go to next zone . '); window.location.href='/home/admin_login' </script>");
+            }
+
+            List<Table_Feedback> lst = null;
+            lst = db.Table_Feedback.ToList();
+
+            return View(lst);
+        }
+
+        public ActionResult ChangePassword()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Log In first. Then go to next zone . '); window.location.href='/home/admin_login' </script>");
+            }
+
+            return View();
+        }
+
+       [HttpPost]
+
+        public ActionResult ChangePassword(string Old_Password, string New_Password, string Confirm_Password)
+        {
+
+            if (New_Password == Confirm_Password && New_Password != Old_Password)
+            { 
+                string Email = Session["aid"].ToString();
+
+                Table_Login lg = db.Table_Login.Where(d => d.Password == Old_Password && d.Email == d.Email).SingleOrDefault();
+                lg.Password = New_Password;
+                db.SaveChanges();
+                Response.Write("<script>alert('Your password changed '); window.location.href='/home/Admin_login' </script>");
+            }
+          
+           else
+            {
+                Response.Write("<script>alert('Please enter valid password '); </script>");            
+            }
+            return View();
+        }
+      
+
+        public void Logout()
+        {
+          
+            Session.Abandon();
+            Response.Write("<script>alert('Log Out'); window.location.href='/home/Admin_login' </script>");
+        }
+
+        public void Delete()
+        {
+            try
+            {
+                string m = Request.QueryString["m"];
+                Table_Registration tbl = db.Table_Registration.SingleOrDefault(x => x.Email == m);
+                db.Table_Registration.Remove(tbl);
+                db.SaveChanges();
+                Response.Write("<script>alert(' Record Successfully deleted '); window.location.href='/admin/RegistrationMGMT' </script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Something went wrong ! '); window.location.href='/admin/RegistrationMGMT' </script>");            
+            
+            }
+
+        }
+        [HttpGet]
+        public ActionResult UpdateRecord()
+        {
+            string aid = Request.QueryString["U"];
+            Table_Registration reg = db.Table_Registration.SingleOrDefault(a => a.Email == aid);
+            return View(reg);
+        }
+
+        [HttpPost]
+        public void UpdateRecord(Table_Registration reg, string Email)
+        {
+            Table_Registration rg = db.Table_Registration.SingleOrDefault(a => a.Email == Email);
+            try
+            {
+                HttpPostedFileBase file = Request.Files["Profile_Picture"];
+                if (file.FileName != "")
+                {
+                    rg.Name = reg.Name;
+                    rg.Mobile = reg.Mobile;
+                    rg.Profile_Picture = file.FileName;
+                    rg.DOB = reg.DOB;
+                    rg.Password = reg.Password;
+                    rg.Address = reg.Address;
+                    rg.Father_Name = reg.Father_Name;
+                    rg.Registration_Date = DateTime.Now.ToString();
+                    rg.Confirm_Password = reg.Confirm_Password;
+
+                    db.SaveChanges();
+                    file.SaveAs(Server.MapPath("~/content/Profile_Pictures/" + file.FileName));
+                    Response.Write("<script>alert('Record Updated Successfully ! ');  window.location.href='/admin/RegistrationMGMT' </script>");
+
+                }
+                else
+                {
+                    Table_Registration dd = db.Table_Registration.SingleOrDefault(x => x.Email == Email);
+
+                    dd.Name = reg.Name;
+                    dd.Mobile = reg.Mobile;
+                //    rg.Profile_Picture = file.FileName;
+                    dd.DOB = reg.DOB;
+                    dd.Password = reg.Password;
+                    dd.Address = reg.Address;
+                    dd.Father_Name = reg.Father_Name;
+                    dd.Registration_Date = DateTime.Now.ToString();
+                    dd.Confirm_Password = reg.Confirm_Password;
+
+                    db.SaveChanges();
+                //    file.SaveAs(Server.MapPath("~/content/Profile_Pictures/" + file.FileName));
+                    Response.Write("<script>alert('Record Updated Successfully ! ');  window.location.href='/admin/RegistrationMGMT' </script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Record Not Updated ! ');  </script>");            
+            
+            }
+        }
+
+    }
+}
---
+++ E_Village/Controllers/HomeController.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_Village.Models;
+
+namespace E_Village.Controllers
+{
+    public class HomeController : Controller
+    {
+        E_VillageEntities db = new E_VillageEntities();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Table_Contact con)
+        {
+            try
+            {
+                con.Contact_Date = DateTime.Now.ToString();
+
+                db.Table_Contact.Add(con);
+                db.SaveChanges();
+                Response.Write("<script>alert('Congratulations ! We will contact you in 24 hours. ')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Something went wrong ! Please fill the form again carefully. ')</script>");
+            }
+            return View();
+        }
+        public ActionResult About_Village()
+        {
+            return View();
+        }
+        public ActionResult Photo_Gallery()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(Table_Registration reg, string hdn1, string ct1)
+        {
+            try
+            {
+                if (hdn1 == ct1)
+                {
+                    HttpPostedFileBase file = Request.Files["Profile_Picture"];
+                    reg.Profile_Picture = file.FileName;
+                    file.SaveAs(Server.MapPath("~/content/Profile_Pictures/" + file.FileName));
+
+
+                    reg.Registration_Date = DateTime.Now.ToString();
+
+                    db.Table_Registration.Add(reg);
+                    db.SaveChanges();
+                    Response.Write("<script>alert('Registration Successful ! '); window.location.href='/home/Public_Login'</script>");
+                }
+
+                else
+                {
+                    Response.Write("<script>alert(' Enter captcha carefully ! ')</script>");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Something went wrong ! Please fill the form again carefully. ')</script>");
+            }
+            return View();
+        }
+      
+        public ActionResult Forget()
+        {
+            return View();
+        }
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Feedback(Table_Feedback fee)
+        {
+            try
+            {
+                fee.Feedback_Date = DateTime.Now.ToString();
+
+                db.Table_Feedback.Add(fee);
+                db.SaveChanges();
+                Response.Write("<script>alert('Thankyou for feedback  ')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Something went wrong ! Please fill the form again carefully. ')</script>");
+            }
+            return View();
+        }
+        public ActionResult Admin_Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Admin_Login(Table_Login lg)
+        {
+            try
+            {
+                Table_Login t1 = db.Table_Login.Where(x=>x.Email==lg.Email && x.Password==lg.Password).SingleOrDefault();
+                if (t1 != null)
+                {
+                    Session["aid"]=lg.Email;                //  set of session  
+
+                    Response.Write("<script>alert(' Welcome to Admin Zone  '); window.location.href='/admin/index' </script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Email ID or Password ! ')</script>");                    
+                }
+            }
+            catch (Exception ex) {
+                Response.Write("<script>alert('There are Error in code !  ! ')</script>");
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact_Us()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact_Us(Table_Contact con)
+        {
+            try
+            {
+                con.Contact_Date = DateTime.Now.ToString();
+
+                db.Table_Contact.Add(con);
+                db.SaveChanges();
+                Response.Write("<script>alert('Congratulations ! We will contact you in 24 hours. ');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Something went wrong ! Please fill the form again carefully. ')</script>");
+            }
+            return View();
+        }
+
+        public ActionResult Public_Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Public_Login(Table_Registration lg)
+        {
+            try
+            {
+                Table_Registration t1 = db.Table_Registration.Where(x => x.Email == lg.Email && x.Password == lg.Password).SingleOrDefault();
+                if (t1 != null)
+                {
+                    Session["aid"] = lg.Email;                //  set of session  
+
+                    Response.Write("<script>alert(' Welcome to Your Dashboard  '); window.location.href='/home/User_Dashboard' </script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Email ID or Password ! ')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('There are Error in code !   ')</script>");
+            }
+
+            return View();
+        }
+
+
+        public ActionResult User_Dashboard()
+        {
+            if (Session["aid"] != null)
+            {
+                // Retrieve the email of the currently logged-in user
+                string userEmail = Session["aid"].ToString();
+
+                // Retrieve the user's data from the database based on the email
+                Table_Registration user = db.Table_Registration.SingleOrDefault(u => u.Email == userEmail);
+
+                if (user != null)
+                {
+                    // Pass the user object to the view
+                    return View(user);
+                }
+                else
+                {
+                    // Handle the case where the user's data is not found
+                    return HttpNotFound();
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('Log In first. Then go to User Dashboard . '); window.location.href='/home/Public_login' </script>");
+            }
+           List<Table_Registration> lst = null;
+            lst = db.Table_Registration.ToList();
+
+           return View(lst);
+        }
+
+    }
+}
